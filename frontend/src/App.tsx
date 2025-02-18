57541b7cbf1bbddba3de71a054c220fb941f7b3d
IsaacAu11 <iszac987@gmail.com>
2025-02-18 18:37:07 +0000

    updated with inital timegrid and fixed timeslot size

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './styles/fullcalendar.css';
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -6,17 +7,29 @@ import interactionPlugin from "@fullcalendar/interaction";
 
 const App: React.FC = () => {
   return (
-    <div style={{ padding: "20px" }}>
-      <h2>FullCalendar</h2>
+    <div className="p-5 h-screen fixed top-0 bottom-0 left-0 right-0">
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        initialView="dayGridWeek"
+        initialView="timeGridWeek"
         editable={true}
         selectable={true}
+        height="95%"
+        timeZone="GMT"
+        headerToolbar={{
+          left: 'prev,next,today',
+          center: 'title',
+          right: ''
+        }}
         events={[
           { title: "Event 1", date: "2025-02-20" },
           { title: "Event 2", date: "2025-02-22" },
         ]}
+        views={{
+          timeGridWeek: {
+            type: 'timeGrid',
+            slotDuration: '01:00:00',
+          }
+        }}
       />
     </div>
   );
