2b705422f21ff5ef714ed559508bc6bbeee36bf2
Jeet <83248565+Lemony-Juicy@users.noreply.github.com>
2025-02-19 22:07:16 +0000

    Database stuff

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,86 @@
-from typing import Union
+from datetime import datetime
+import logging  # To debug -> Can't print, as the file isn't executed normally
 
-from fastapi import FastAPI
+# DB stuff
+from sqlalchemy.orm import Session
+from database import models, SessionLocal, engine, ORM_Base
+
+# FastAPI stuff
+from fastapi import FastAPI, Depends, Request, Form, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+logger = logging.getLogger('uvicorn.error')
+logger.setLevel(logging.DEBUG)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Allow frontend (js) to communicate with backend with CORS (Cross-Origin Resource Sharing)
+# Middleware is a function that is passed through every request before it's passed through a path operation
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Update with frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+ORM_Base.metadata.create_all(bind=engine)  # Create tables
+
+# This creates new user db session each request
+# A global db var introduces issues with multiple users accessing same db session
+def yield_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+
+@app.get("/", name="home")
+def home_page(request: Request, db: Session = Depends(yield_db)):
+    user_tasks = db.query(models.Task).all()
+    logger.debug(user_tasks)
+    logger.debug(len(user_tasks))
+    return templates.TemplateResponse("home.html", {"request": request, "user_tasks": user_tasks})
+
+
+# title is expected as a form parameter (purpose of ellipses ...)
+@app.post("/add", response_class=HTMLResponse)
+def add(request: Request, title: str = Form(...), priority: int = Form(...), deadline: datetime = Form(...), db: Session = Depends(yield_db)):
+    new_task = models.Task(title=title, priority=priority, start=datetime.now(), end=deadline)
+    db.add(new_task)
+    db.commit()
+    
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
+
+
+@app.get("/delete/{task_id}")
+def delete(request: Request, task_id: int, db: Session = Depends(yield_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    db.delete(task)
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+
+
+@app.get("/update/{task_id}")
+def update(request: Request, task_id: int, db: Session = Depends(yield_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    task.completed = not task.completed
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+    
+    
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5173)
