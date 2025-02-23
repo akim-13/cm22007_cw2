00813d3e425dcd54bf48a8dfcbb4447bd488eae0
IsaacAu11 <iszac987@gmail.com>
2025-02-23 22:20:25 +0000

    moved calendar to a separate file and updated css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,11 @@
 import React from "react";
 import './styles/fullcalendar.css';
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
+import Calendar from './components/Calendar';
 
 const App: React.FC = () => {
   return (
     <div className="p-5 h-screen fixed top-0 bottom-0 left-0 right-0">
-      <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        initialView="timeGridWeek"
-        editable={true}
-        selectable={true}
-        height="95%"
-        timeZone="local"
-        headerToolbar={{
-          left: 'prev,next,today',
-          center: 'title',
-          right: ''
-        }}
-        events={'https://fullcalendar.io/api/demo-feeds/events.json'}
-        views={{
-          timeGridWeek: {
-            type: 'timeGrid',
-            slotDuration: '01:00:00',
-            slotLabelInterval: '01:00:00',
-          }
-        }}
-        scrollTime={'09:00:00'}
-        eventBorderColor="white"
-        eventColor="rgb(59,130,246)"
-      />
+      <Calendar />
     </div>
   );
 };
