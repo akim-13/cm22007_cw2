059d9b8db2584e17e6c8a91bd36a2ca5a9b3cdc8
IsaacAu11 <iszac987@gmail.com>
2025-03-12 22:10:18 +0000

    fixed calendar.tsx

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -1,153 +1,163 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import axios from "axios";
 import AchievementModal from "./AchievementModal";
 import SettingsModal from "./SettingsModal";
 
 const formatDate = (date: Date): string => {
-  return date.toLocaleString("en-US", {
-    month: "2-digit",
-    day: "2-digit",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-  });
+    return date.toLocaleString("en-US", {
+        month: "2-digit",
+        day: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+    });
 };
 
-// Add setIsModalOpen to the component props
-const Calendar: React.FC<any> = ({
-  events,
-  setIsModalOpen,
-  newFCEvent,
-  initialExtendedProps,
-}) => {
-  const [isAchievementModalOpen, setIsAchievementModalOpen] = useState(false);
-  const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
+interface TaskEvent {
+    eventID: string;
+    title: string;
+    start: string;
+    extendedProps: {
+        description: string;
+        priority: string;
+        duration: string;
+        isCompleted: boolean;
+    };
+}
 
-  const handleAchievementsClick = () => {
-    setIsAchievementModalOpen(true);
-  };
+interface StandaloneEvent {
+    eventID: string;
+    title: string;
+    start: string;
+    end: string;
+}
 
-  const handleSettingsClick = () => {
-    //Add your settings logic here
-    setIsSettingsModalOpen(true);
-  };
+const Calendar: React.FC<any> = ({ events, setIsModalOpen, newFCEvent, initialExtendedProps }) => {
+    const [isAchievementModalOpen, setIsAchievementModalOpen] = useState(false);
+    const [backendEvents, setBackendEvents] = useState<StandaloneEvent[]>([]);
+    const [taskEvents, setTaskEvents] = useState<TaskEvent[]>([]);
+    const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
+    const username = "joe"; 
+    useEffect(() => {
+        fetchEvents();
+        fetchTasks();
+    }, []);
 
-  return (
-    <>
-      <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        initialView="timeGridWeek"
-        editable={true}
-        selectable={true}
-        height="90%"
-        timeZone="local"
-        headerToolbar={{
-          left: "prev,next,today,title",
-          center: "",
-          right: "achievements,settings",
-        }}
-        titleFormat={{ month: "short", day: "numeric", year: "numeric" }}
-        customButtons={{
-          achievements: {
-            text: "Achievements",
-            click: handleAchievementsClick,
-          },
-          settings: {
-            text: "Settings",
-            click: handleSettingsClick,
-          },
-        }}
-        eventSources={[
-          {
-            url: "https://fullcalendar.io/api/demo-feeds/events.json",
-            color: "rgb(59,130,246)",
-            textColor: "white",
-          },
-          {
-            events: events,
-            color: "rgb(255,99,132)",
-            textColor: "black",
-          },
-        ]}
-        eventClick={(info) => {
-          const startTime = info.event.start
-            ? info.event.start.toLocaleString("en-US", {
-                weekday: "long",
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-                hour: "2-digit",
-                minute: "2-digit",
-              })
-            : "No start time";
+    const fetchEvents = async () => {
+        try {
+            const standaloneEventsResponse = await axios.get(
+                `http://localhost:8000/get_standalone_events/${username}`
+            );
+            const standaloneEvents = standaloneEventsResponse.data.standalone_events.map(
+                (event: any) => ({
+                    eventID: event.standaloneEventID,
+                    title: event.standaloneEventName,
+                    start: event.start,
+                    end: event.end,
+                })
+            );
+            setBackendEvents([...standaloneEvents]);
+        } catch (error) {
+            console.error("Error fetching events:", error);
+        }
+    };
 
-          const endTime = info.event.end
-            ? info.event.end.toLocaleString("en-US", {
-                weekday: "long",
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-                hour: "2-digit",
-                minute: "2-digit",
-              })
-            : "No end time";
-          const extendedProps = info.event.extendedProps;
-          let extendedPropsText = "";
+    const fetchTasks = async () => {
+        try {
+            const taskResponse = await axios.get(`http://localhost:8000/get_user_tasks/${username}`);
+            console.log("Tasks fetched from API:", taskResponse.data.tasks); 
+            const tasks = taskResponse.data.tasks.map((task: any) => ({
+                id: task.taskID,
+                title: task.title,
+                start: new Date(task.deadline).toISOString(),
+                extendedProps: {
+                    description: task.description,
+                    priority: task.priority,
+                    duration: task.duration,
+                    isCompleted: task.isCompleted,
+                },
+            }));
+            setTaskEvents(tasks);
+        } catch (error) {
+            console.error("Error fetching tasks:", error);
+        }
+    };
 
-          if (extendedProps) {
+    const handleEventClick = (info: any) => {
+        const startTime = info.event.start ? formatDate(info.event.start) : "No start time";
+        const endTime = info.event.end ? formatDate(info.event.end) : "No end time";
+        const extendedProps = info.event.extendedProps;
+        let extendedPropsText = "";
+
+        if (extendedProps) {
             extendedPropsText = Object.entries(extendedProps)
-              .map(([key, value]) => `${key}: ${value}`)
-              .join("\n");
-          }
+                .map(([key, value]) => `${key}: ${value}`)
+                .join("\n");
+        }
 
-          // Reset the current event
-          newFCEvent.current = { extendedProps: { ...initialExtendedProps } };
+        newFCEvent.current = { extendedProps: { ...initialExtendedProps } };
+        newFCEvent.current.title = info.event.title || "";
+        newFCEvent.current.start = startTime;
+        newFCEvent.current.end = endTime;
+        newFCEvent.current.extendedProps = extendedProps;
+        setIsModalOpen(true);
+    };
 
-          const currentEvent = info.event;
+    return (
+        <>
+            <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                initialView="timeGridWeek"
+                editable={true}
+                selectable={true}
+                height="90%"
+                timeZone="local"
+                headerToolbar={{
+                    left: "prev,next,today,title",
+                    center: "",
+                    right: "achievements,settings",
+                }}
+                titleFormat={{ month: "short", day: "numeric", year: "numeric" }}
+                customButtons={{
+                    achievements: {
+                        text: "Achievements",
+                        click: () => setIsAchievementModalOpen(true),
+                    },
+                    settings: {
+                        text: "Settings",
+                        click: () => setIsSettingsModalOpen(true),
+                    },
+                }}
+                eventSources={[
+                    {
+                        events: [...taskEvents],
+                        color: "rgb(144,238,144)",
+                        textColor: "black",
+                    },
+                    {
+                        events: [ ...backendEvents],
+                        color: "rgb(255,99,132)",
+                        textColor: "black",
+                    }
+                ]}
+                eventClick={handleEventClick}
+            />
 
-          newFCEvent.current.title = formatDate(currentEvent?.title || "");
-          newFCEvent.current.start = formatDate(currentEvent?.start || "");
-          newFCEvent.current.end = formatDate(info.event?.end || "");
-          newFCEvent.current.extendedProps.duration =
-            currentEvent?.extendedProps.duration || "";
-          newFCEvent.current.extendedProps.priority =
-            currentEvent?.extendedProps.priority || "";
-          newFCEvent.current.extendedProps.description =
-            currentEvent?.extendedProps.description || "";
-          newFCEvent.current.extendedProps.isCompleted =
-            currentEvent?.extendedProps.isCompleted || false;
-
-          setIsModalOpen(true);
-
-          // alert(`Event: ${info.event.title}\nID: ${info.event.id}\nStarts: ${startTime}\nEnds: ${endTime}\n---\nExtended props:\n${extendedPropsText}`);
-        }}
-        views={{
-          timeGridWeek: {
-            type: "timeGrid",
-            slotDuration: "01:00:00",
-            slotLabelInterval: "01:00:00",
-          },
-        }}
-        scrollTime={"09:00:00"}
-        eventBorderColor="white"
-        eventColor="rgb(59,130,246)"
-        nowIndicator={true}
-      />
-
-      <AchievementModal
-        isOpen={isAchievementModalOpen}
-        onClose={() => setIsAchievementModalOpen(false)}
-      />
-      <SettingsModal
-        open={isSettingsModalOpen}
-        onClose={() => setIsSettingsModalOpen(false)}
-      />
-    </>
-  );
+            <AchievementModal
+                isOpen={isAchievementModalOpen}
+                onClose={() => setIsAchievementModalOpen(false)}
+            />
+            <SettingsModal
+                open={isSettingsModalOpen}
+                onClose={() => setIsSettingsModalOpen(false)}
+             />
+        </>
+    );
 };
 
 export default Calendar;
